97ca7fa1626a11ec0a5181bc9976cdcc34d7fed8
igorjizn <killingasyringe@gmail.com>
2024-04-29 23:56:25 +0300

    KS-1-1 create two variables

---
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -0,0 +1,6 @@
+package org.example.lesson1
+
+fun main() {
+    var numberOfOrders: Byte = 75 //var потому что количество заказов должно меняться
+    val thankYouText: String = "Благодарим за покупку"
+}
